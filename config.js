a54610f984ac37424b112e26eca5396ebe400f5b
lechodiman <lechodiman@uc.cl>
2020-02-15 00:06:04 -0300

    chore(config.js): add google analytics

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   copyright: '© Todos los derechos reservados.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-158614211-1',
   useKatex: false,
   menu: [
     {
